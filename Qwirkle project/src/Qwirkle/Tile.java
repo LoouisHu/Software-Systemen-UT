876c77d8bd1a274f15d1afa18dae6931b43df56a
Louis <pokingotaku@gmail.com>
2016-01-11 16:37:04 +0100

    Finished Tile.java

--- Qwirkle project/src/Qwirkle/Tile.java
+++ Qwirkle project/src/Qwirkle/Tile.java
@@ -2,8 +2,52 @@ package Qwirkle;
 
 public class Tile {
 
-	public Tile() {
-		// TODO Auto-generated constructor stub
+	public static enum Color {
+		RED('R'), ORANGE('O'), BLUE('B'), YELLOW('Y'), GREEN('G'), PURPLE('P');
+		public final char c;
+
+		Color(char ch) {
+			this.c = ch;
+		}
 	}
 
+	public static enum Shape {
+		CIRCLE('o', '\u262f'), DIAMOND('d', '\u2615'), SQUARE('s', '\u25A0'), CLOVER('c','\u269C'), CROSS('x', '\u2693'), STAR('*', '\u2605');
+		public final char c;
+		public final char u;
+		
+		Shape(char c, char u){
+			this.c = c;
+			this.u = u;
+		}
+	}
+	
+	private final Shape shape;
+	private final Color color;
+	
+	public Tile(Color color, Shape shape){
+		this.shape = shape;
+		this.color = color;
+	}
+	
+
+	public Color getColor(){
+		 return color;
+	}
+	
+	public Shape getShape(){
+		return shape;
+	}
+	
+	public String toString(){
+		return String.valueOf(shape.u);
+	}
+	
+	public static void main(String[] args){
+		Shape s = Shape.CIRCLE;
+		Color c = Color.RED;
+		Tile t = new Tile(c, s);
+		System.out.println(c.toString() + t.toString());
+	}
+	
 }
