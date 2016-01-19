19418a51c3647dde7a5f969ab9c6b37b4ecaf65c
Louis <pokingotaku@gmail.com>
2016-01-19 14:44:23 +0100

    Added HumanPlayer and RetardedStrategy

---
+++ Qwirkle project/src/player/HumanPlayer.java
@@ -0,0 +1,14 @@
+package player;
+
+import java.util.Set;
+
+import Qwirkle.Tile;
+
+public class HumanPlayer extends Player {
+
+	public HumanPlayer(String name, Set<Tile> hand) {
+		super(name, hand);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Qwirkle project/src/player/RetardedStrategy.java
@@ -0,0 +1,29 @@
+package player;
+
+import java.util.List;
+
+import Qwirkle.Board;
+import Qwirkle.Move;
+//TODO bitches vragen over de determinemove
+public class RetardedStrategy implements Strategy {
+	
+	private Player player;
+
+	public RetardedStrategy(Player player) {
+		this.player = player;
+	}
+
+	@Override
+	public String determineMove(Board board) {
+		// TODO Auto-generated method stub
+		
+		return null;
+	}
+
+	@Override
+	public List<Move> strategyPlay(Board board) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
