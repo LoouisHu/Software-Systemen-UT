593c9141258a12be695bc71061843dfd20fd2562
Louis <pokingotaku@gmail.com>
2016-01-26 12:39:26 +0100

    Fixed checkstyles in Server

--- Qwirkle project/src/controller/TileBag.java
+++ Qwirkle project/src/controller/TileBag.java
@@ -10,72 +10,79 @@ import model.Tile;
 
 //TODO gebruiken voor de server;
 public class TileBag {
-	
-	//@ private invariant tiles != null;
-	
+
+	// @ private invariant tiles != null;
+
 	// Aantal stenen
 	public static final int SIZE = 108;
-			
-	//Aantal stenen in hand
+
+	// Aantal stenen in hand
 	public static final int SIZE_HAND = 6;
-	
-	//Tiles dat in de bag zitten
+
+	// Tiles dat in de bag zitten
 	private ArrayList<Tile> tiles;
-	
+
 	/**
 	 * Maakt een nieuwe bag en vult het met de 108 tiles.
 	 */
+
 	public TileBag() {
 		this.tiles = new ArrayList<>(SIZE);
-		
-		for (Tile.Color c : Tile.Color.values()){
-			for( Tile.Shape s : Tile.Shape.values()){
-				for(int i = 0; i < 3; i++) {
+		for (Tile.Color c : Tile.Color.values()) {
+			for (Tile.Shape s : Tile.Shape.values()) {
+				for (int i = 0; i < 3; i++) {
 					this.tiles.add(new Tile(c, s));
 				}
 			}
 		}
 	}
+
 	/**
 	 * Returns a list of random tiles.
-	 * @param amount Amount of tiles to return
+	 * 
+	 * @param amount
+	 *            Amount of tiles to return
 	 */
-	//@ requires amount > 0 && amount <= Player.SIZE_HAND;
-	//@ ensures \result != null;
-	//@ ensures \result.size() == amount;
-	//@ ensures getSize() == (amount >= getSize() ? \old(getSize()) - amount : 0);
-	public List<Tile> getTiles(int amount){
+	// @ requires amount > 0 && amount <= Player.SIZE_HAND;
+	// @ ensures \result != null;
+	// @ ensures \result.size() == amount;
+	// @ ensures getSize() == (amount >= getSize() ? \old(getSize()) - amount :
+	// 0);
+	public List<Tile> getTiles(int amount) {
 		List<Tile> hand = new ArrayList<Tile>();
-		for (int i = 0; i < amount && getTileBagSize() > 0; i++){
+		for (int i = 0; i < amount && getTileBagSize() > 0; i++) {
 			hand.add(tiles.get(0));
 			tiles.remove(0);
 		}
 		return hand;
 	}
-	
-	//shuffles the tiles
-	public void shuffle(){
+
+	// shuffles the tiles
+	public void shuffle() {
 		Collections.shuffle(this.tiles);
 	}
-	
-	
-	public int getTileBagSize(){
+
+	public int getTileBagSize() {
 		return this.tiles.size();
 	}
-	
+
 	/**
 	 * Kijkt of de bag leeg is.
+	 * 
 	 * @return
 	 */
-	public boolean isEmpty(){
+	public boolean isEmpty() {
 		return this.tiles.isEmpty();
 	}
+
 	/**
 	 * Keert de tiles weer terug in de stapel en schudt ze.
-	 * @param tiles de tiles die je in je hand had oid.
+	 * 
+	 * @param tiles
+	 *            de tiles die je in je hand had oid.
 	 */
-	//@ requires tiles != null;
-	//@ ensures getSize() == \old(getSize()) + tiles.size();
+	// @ requires tiles != null;
+	// @ ensures getSize() == \old(getSize()) + tiles.size();
 	public void returnTiles(List<Tile> t) {
 		tiles.addAll(t);
 		shuffle();
--- Qwirkle project/src/view/TUI.java
+++ Qwirkle project/src/view/TUI.java
@@ -17,7 +17,6 @@ import model.Tile;
 import player.HumanPlayer;
 import player.Player;
 
-
 public class TUI extends Observable implements Runnable {
 	private int playerNumber;
 	private Socket sock;
@@ -27,7 +26,7 @@ public class TUI extends Observable implements Runnable {
 	private String name;
 	private Board board;
 	private Player player;
-	
+
 	public TUI() {
 		String ip = "";
 		String port = "";
@@ -35,64 +34,66 @@ public class TUI extends Observable implements Runnable {
 		InetAddress inet = null;
 		int portnr = 0;
 		board = new Board();
-		
+
 		scan = new Scanner(System.in);
 		System.out.print("IP: ");
-		if(scan.hasNext()) {
+		if (scan.hasNext()) {
 			ip = scan.next();
 		}
 		System.out.print("Port: ");
-		if(scan.hasNext()) {
+		if (scan.hasNext()) {
 			port = scan.next();
 		}
 		System.out.print("Name: ");
-		if(scan.hasNext()) {
+		if (scan.hasNext()) {
 			name = scan.next();
 		}
-		
+
 		try {
 			inet = InetAddress.getByName(ip);
-		} catch(UnknownHostException u) {
+		} catch (UnknownHostException u) {
 			System.out.println("Ip-adres fout. Voer een juist ip-adres in.");
 		}
-		
+
 		try {
 			portnr = Integer.parseInt(port);
-		} catch(NumberFormatException n) {
+		} catch (NumberFormatException n) {
 			System.out.println("Port nummer fout.");
 		}
-		
+
 		try {
-			if(inet != null && inet != null) {
+			if (inet != null) {
 				sock = new Socket(inet, portnr);
 			}
 		} catch (IOException e) {
 			System.out.println("Socketfout. Voer het juiste ip-adres en de juiste port nummer in.");
 		}
-		
-		if(sock != null) {
+
+		if (sock != null) {
 			try {
 				in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
 				out = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream()));
 			} catch (IOException e) {
-				System.out.println("Socketfout. Voer het juiste ip-adres en de juiste port nummer in.");
+				System.out.println("Socketfout. Voer het juiste ip-adres en port nummer in.");
 			}
-		
+
 			sendHello(name);
 			new Thread(this).start();
 		}
 	}
-	
+
 	/**
 	 * Stuurt een Protocol.JOIN met een bericht.
+	 * 
 	 * @require message != null
 	 */
 	public void sendHello(String name) {
 		sendMessage(Protocol.HELLO + " " + name);
 	}
-	
+
 	/**
 	 * Hiermee kan de Client-kant een bericht sturen naar de server.
+	 * 
 	 * @require message != null
 	 */
 	public void sendMessage(String message) {
@@ -104,16 +105,18 @@ public class TUI extends Observable implements Runnable {
 			shutdown();
 		}
 	}
-	
+
 	/**
-	 * Ontvangt berichten van de server en stuurt ze door naar verwerk(message) om de berichten te verwerken. Leest per regel.
+	 * Ontvangt berichten van de server en stuurt ze door naar verwerk(message)
+	 * om de berichten te verwerken. Leest per regel.
+	 * 
 	 * @require in != null
 	 */
 	public void run() {
 		try {
-			while(true) {
+			while (true) {
 				String message = in.readLine();
-				if(message != null) {
+				if (message != null) {
 					verwerk(message);
 				}
 			}
@@ -121,68 +124,68 @@ public class TUI extends Observable implements Runnable {
 			shutdown();
 		}
 	}
-	
+
 	public void verwerk(String message) {
 		Scanner sc = new Scanner(message);
 		String commando = null;
-		if(sc.hasNext()) {
+		if (sc.hasNext()) {
 			commando = sc.next();
 		}
-		switch(commando) {
-		case Protocol.WELCOME:
-			if(sc.hasNext()) {
-				name = sc.next();
-				player = new HumanPlayer(name, board);
-			}
-			if(sc.hasNext()) {
-				playerNumber = Integer.parseInt(sc.next());
-			}
-			break;
-		case Protocol.WINNER:
-			if(sc.hasNext()) {
-				System.out.println("The winner: " + sc.next());
-			}
-			break;
-		case Protocol.NEXT:
-			if(sc.hasNext()) {
-				String number = sc.next();
-				if(playerNumber == Integer.parseInt(number)) {
-					System.out.println("Your turn");
-					player.makeMove();
-				} else {
-					System.out.println("Next player: " + player);
+		switch (commando) {
+			case Protocol.WELCOME:
+				if (sc.hasNext()) {
+					name = sc.next();
+					player = new HumanPlayer(name, board);
 				}
-			}
-			break;
+				if (sc.hasNext()) {
+					playerNumber = Integer.parseInt(sc.next());
+				}
+				break;
+			case Protocol.WINNER:
+				if (sc.hasNext()) {
+					System.out.println("The winner: " + sc.next());
+				}
+				break;
+			case Protocol.NEXT:
+				if (sc.hasNext()) {
+					String number = sc.next();
+					if (playerNumber == Integer.parseInt(number)) {
+						System.out.println("Your turn");
+						player.makeMove();
+					} else {
+						System.out.println("Next player: " + player);
+					}
+				}
+				break;
 		}
 	}
-	
+
 	/**
 	 * De socket wordt afgesloten.
 	 */
 	public void shutdown() {
 		try {
-			if(sock != null) {
+			if (sock != null) {
 				sock.close();
 			}
 		} catch (IOException e) {
 			System.out.println("Fout bij socket sluiten.");
 		}
 	}
-	
-	public void displayHand(Tile[] hand){
+
+	public void displayHand(Tile[] hand) {
 		String handString = "Your hand is:";
-		for (Tile t : hand){
+		for (Tile t : hand) {
 			handString = handString.concat(" " + t.toString());
 		}
 		System.out.println(handString);
 	}
-	public String displayBoard(String board){
+
+	public String displayBoard(String board) {
 		board = board.toString();
 		return board;
 	}
-	
-	
+
 	public static void main(String[] args) {
 		new TUI();
 	}
